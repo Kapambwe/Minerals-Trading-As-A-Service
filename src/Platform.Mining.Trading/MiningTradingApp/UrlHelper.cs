00aaa3e535afb4c0eeb53b2cc13c8f01dc7654f2
Abraham Kapambwe <AbrahamKapambwe@neosoftapp.onmicrosoft.com>
2025-10-12 09:05:40 +0200

    changes

---
+++ src/Platform.Mining.Trading/MiningTradingApp/UrlHelper.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MiningTradingApp
+{
+    public static class UrlHelper
+    {
+        public static string WithBase(this NavigationManager nav, string relative)
+            => $"{nav.BaseUri}{relative.TrimStart('/')}";
+    }
+}
